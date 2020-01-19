2f6e88bc81c18cb356852b833dc36d59cc316c95
Rohan Skariah <rohanskariah@outlook.com>
2020-01-19 08:11:12 -0800

    More interface stuff, almost done

--- DrawFigures.js
+++ DrawFigures.js
@@ -1,38 +1,42 @@
 var canvas = document.querySelector('canvas');
 
-canvas.width = window.innerWidth / 2 ;
-canvas.height = window.innerHeight / 3 * 2;
+canvas.width = 1000;
+canvas.height = 500;
 
 var c = canvas.getContext('2d');
 const gridFactor = 10;
 
+function drawGrid()
+{
+    // Grid 
+    var xGrid = 0;
+    var yGrid = 0;
+    c.globalAlpha = 0.2;
+    for (var i = 0; i < canvas.width / gridFactor; i++)
+    {
+        c.beginPath();
+        c.moveTo(xGrid, 0);
+        c.lineTo(xGrid, canvas.height);
+        c.strokeStyle = "black";
+        c.stroke();
+        xGrid += gridFactor * 5;
+    }
+    for (var j = 0; j < canvas.height / gridFactor; j++)
+    {
+        c.beginPath();
+        c.moveTo(0, yGrid);
+        c.lineTo(canvas.width, yGrid);
+        c.strokeStyle = "black";
+        c.stroke();
+        yGrid += gridFactor * 5;
+    }
+    c.globalAlpha = 1;;
+}
 
-// function drawGrid()
-// {
-//     // Grid 
-//     var xGrid = 0;
-//     var yGrid = 0;
-//     c.globalAlpha = 0.2;
-//     for (var i = 0; i < canvas.width / gridFactor; i++)
-//     {
-//         c.beginPath();
-//         c.moveTo(xGrid, 0);
-//         c.lineTo(xGrid, canvas.height);
-//         c.strokeStyle = "black";
-//         c.stroke();
-//         xGrid += gridFactor * 5;
-//     }
-//     for (var j = 0; j < canvas.height / gridFactor; j++)
-//     {
-//         c.beginPath();
-//         c.moveTo(0, yGrid);
-//         c.lineTo(canvas.width, yGrid);
-//         c.strokeStyle = "black";
-//         c.stroke();
-//         yGrid += gridFactor * 5;
-//     }
-//     c.globalAlpha = 1;;
-// }
+function clearCanvas()
+{
+    c.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 
 
@@ -44,7 +48,7 @@ function drawCircle(radius)
         var yPos = parseInt(canvas.height) / 2 / gridFactor;
 
         c.clearRect(0, 0, canvas.width, canvas.height);
-        //drawGrid();
+        drawGrid();
         c.fillText("Radius = " + radius, gridFactor * (Math.round(radius) + Math.round(xPos)) + 10, yPos * gridFactor);
         
         c.beginPath();
@@ -53,12 +57,12 @@ function drawCircle(radius)
     }
     else
         alert("Values must be non-negative, duh");
-
 }
 
 
 function drawRect(width, height)
 {
+
     if (width < 0 || height < 0)
     {
         alert("Values must be non-negative, duh");
@@ -69,27 +73,76 @@ function drawRect(width, height)
         var xPos = parseInt(canvas.width) / 2 ;
         var yPos = parseInt(canvas.height) / 2 ;
         c.clearRect(0, 0, canvas.width, canvas.height);
-        //drawGrid(); 
+        drawGrid(); 
         c.strokeStyle = "black";
         c.strokeRect(xPos - (width / 2), yPos - (height / 2), width, height);
     }
 }
 
 
-function drawTriangle()
+function drawTriangle(side1, side2, angle)
 {
+    c.strokeStyle = "red";
+    c.strokeRect(0, 0, canvas.width, canvas.height);
+    var side1 = parseInt(side1) * gridFactor;
+    var side2 = parseInt(side2) * gridFactor;
+    var angle = (parseInt(angle) % 360) * Math.PI / 180;
+    //alert("sin  " + Math.round(Math.sin(angle)));
+    //alert("cos  " + Math.round(Math.cos(angle)));
     var xPos1, xPos2, xPos3, yPos1, yPos2, yPos3;
 
-//Line
-c.clearRect(0, 0, canvas.width, canvas.height);
-//drawGrid();
+
+    xPos1 = 0;
+    yPos1 = 0;
+    xPos2 = side1;
+    yPos2 = 0;
+    xPos3 = Math.round(side2 * Math.cos(angle));
+    yPos3 = Math.round(side2 * Math.sin(angle));
+    yPos3 = 2 * yPos1 - yPos3;
+
+    var xAve = (xPos1 + xPos2 + xPos3) / 3;
+    var yAve = (yPos1 + yPos2 + yPos3) / -3;
+    //alert("X Average:" + xAve + " Y Average: " + yAve);
+
+    var xOffset = (canvas.width / 2) - xAve;
+    var yOffset = (canvas.height / 2) + yAve;
+    //alert(xPos1 + " " + yPos1 + " " + xPos2 + " " + yPos2 + " " + xPos3 + " " + yPos3);    
+    
+    //alert("x: " + xOffset + " y: " + yOffset);
+
+    xPos1 += xOffset;
+    xPos2 += xOffset;
+    xPos3 += xOffset;
+    yPos1 += yOffset;
+    yPos2 += yOffset;
+    yPos3 += yOffset;
+    
+    //alert(xPos1 + " " + yPos1 + " " + xPos2 + " " + yPos2 + " " + xPos3 + " " + yPos3);    
+
+
+    //Line
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
 
     c.beginPath();
-    c.moveTo(50, 300);
-    c.lineTo(300, 100);
-    c.lineTo(400, 300);
-    c.lineTo(50, 300);
-    c.strokeStyle = "blue";
+    c.moveTo(xPos1, yPos1);
+    c.lineTo(xPos2, yPos2);
+    c.lineTo(xPos3, yPos3);
+    c.lineTo(xPos1, yPos1);
+    c.strokeStyle = "black";  
+    c.stroke();
+
+    c.beginPath();
+    c.arc(xPos1, yPos1, (xPos2 - xPos1) / 10, 2*Math.PI - angle, 0 , false);
+    c.stroke();
+}
+
+function drawEllipse(majorRadius, minorRadius)
+{
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
+    c.beginPath();
+    c.ellipse(canvas.width / 2, canvas.height / 2, majorRadius * gridFactor, minorRadius * gridFactor, 0, 0, 2 * Math.PI, false);
     c.stroke();
 }
 
--- Interface_buttons.js
+++ Interface_buttons.js
@@ -1,24 +1,39 @@
-
 function tributton() {
-    document.getElementById("circinterface").className = "hidden";
+    document.getElementById("conicinterface").className = "hidden";
     document.getElementById("quadinterface").className = "hidden";
     document.getElementById("triinterface").className = "visible";
+    c.clearRect(0, 0, canvas.width, canvas.height);
 }
 function quadbutton() {
-    document.getElementById("circinterface").className = "hidden";
+    document.getElementById("conicinterface").className = "hidden";
     document.getElementById("triinterface").className = "hidden";
     document.getElementById("quadinterface").className = "visible";
-}
-function circbutton() {
-    document.getElementById("triinterface").className = "hidden";
-    document.getElementById("quadinterface").className = "hidden";
-    document.getElementById("circinterface").className = "visible";
+    c.clearRect(0, 0, canvas.width, canvas.height);
 }
 function displayInputSquare() {
     document.getElementById("rectinterface").className = "hidden";
     document.getElementById("sqinterface").className = "visible";
+    c.clearRect(0, 0, canvas.width, canvas.height);
 }
 function displayInputRectangle() {
     document.getElementById("sqinterface").className = "hidden";
     document.getElementById("rectinterface").className = "visible";
+    c.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function conicButton() {
+    document.getElementById("triinterface").className = "hidden";
+    document.getElementById("quadinterface").className = "hidden";
+    document.getElementById("conicinterface").className = "visible";
+    c.clearRect(0, 0, canvas.width, canvas.height);
+}
+function displayInputCircle() {
+    document.getElementById("circinterface").className = "visible";
+    document.getElementById("ellipseinterface").className = "hidden";
+    c.clearRect(0, 0, canvas.width, canvas.height);
+}
+function displayInputEllipse() {
+    document.getElementById("circinterface").className = "hidden";
+    document.getElementById("ellipseinterface").className = "visible";
+    c.clearRect(0, 0, canvas.width, canvas.height);
 }
