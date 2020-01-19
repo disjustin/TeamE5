713e5dbbfa5a7bd38bed2ba1148e2950c6f14aeb
tangias <tansun@ucdavis.edu>
2020-01-18 21:01:17 -0800

    Triangle Manipulation/Determination

---
+++ Tangia's testing website.js
@@ -0,0 +1,11 @@
+//When right triangle button is pressed
+function rightT()
+    {
+    var RightTriangle = true
+    var EquivTriangle = false
+    var IsoTriangle = false
+    }
+function equivT()
+    {
+        var
+    }
