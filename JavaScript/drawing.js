503aa1775a82b82134fdb741f7afbad024f8bfe8
Rohan Skariah <rohanskariah@outlook.com>
2020-01-19 01:21:28 -0800

    changes

--- JavaScript/drawing.js
+++ JavaScript/drawing.js
@@ -1,41 +1,95 @@
 var canvas = document.querySelector('canvas');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth / 2 ;
+canvas.height = window.innerHeight / 3 * 2;
 
 var c = canvas.getContext('2d');
+const gridFactor = 10;
 
-function drawCircle(radius, xPos, yPos)
+
+function drawGrid()
 {
-    c.fillText("Radius = " + radius, Math.round(radius) + Math.round(xPos), yPos);
-
-    c.beginPath();
-    c.arc(xPos, yPos, radius, 0, 2* Math.PI , true);
-    c.stroke();
-
-    //for (var i = 0; i < 10; i++ )
-    //{
-    //   c.beginPath();
-    //    c.arc(Math.random()*window.innerWidth, Math.random()*window.innerHeight, radius++, 0, 2* Math.PI , true);
-    //    c.stroke();
-    //}
+    // Grid 
+    var xGrid = 0;
+    var yGrid = 0;
+    c.globalAlpha = 0.2;
+    for (var i = 0; i < canvas.width / gridFactor; i++)
+    {
+        c.beginPath();
+        c.moveTo(xGrid, 0);
+        c.lineTo(xGrid, canvas.height);
+        c.strokeStyle = "black";
+        c.stroke();
+        xGrid += gridFactor * 5;
+    }
+    for (var j = 0; j < canvas.height / gridFactor; j++)
+    {
+        c.beginPath();
+        c.moveTo(0, yGrid);
+        c.lineTo(canvas.width, yGrid);
+        c.strokeStyle = "black";
+        c.stroke();
+        yGrid += gridFactor * 5;
+    }
+    c.globalAlpha = 1;;
 }
 
 
-function drawRect()
+
+function drawCircle(radius)
 {
-    c.fillStyle = "red";
-    c.fillRect(50, 50, 100, 100);
-    c.fillStyle = "blue";
-    c.fillRect(300, 300, 100, 100);
-    c.fillStyle = "green";
-    c.fillRect(159, 150, 100, 100);
+    if (radius > 0)
+    {
+        var xPos = parseInt(canvas.width) / 2 / gridFactor;
+        var yPos = parseInt(canvas.height) / 2 / gridFactor;
+
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        drawGrid();
+        c.fillText("Radius = " + radius, gridFactor * (Math.round(radius) + Math.round(xPos)) + 10, yPos * gridFactor);
+        
+        c.beginPath();
+        c.arc(xPos * gridFactor, yPos * gridFactor, radius * gridFactor, 0, 2* Math.PI , true);
+        c.stroke();
+    }
+    else
+        alert("Values must be non-negative, duh");
+
+}
+
+
+function drawRect(width, height)
+{
+    if (width < 0 || height < 0)
+    {
+        alert("Values must be non-negative, duh");
+    }
+    else{
+        var width = parseInt(width) * gridFactor;
+        var height = parseInt(height) * gridFactor;
+        var xPos = parseInt(canvas.width) / 2 ;
+        var yPos = parseInt(canvas.height) / 2 ;
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        drawGrid();
+
+        //c.fillText(width + "   " + height, 200, 100);   
+        c.fillStyle = "red";
+        c.fillRect(xPos - (width / 2), yPos - (height / 2), width, height);
+    }
 }
 
 
 function drawTriangle()
 {
+
+    var xPos1, xPos2, xPos3, yPos1, yPos2, yPos3;
+
+
+
+
 //Line
+c.clearRect(0, 0, canvas.width, canvas.height);
+drawGrid();
+
     c.beginPath();
     c.moveTo(50, 300);
     c.lineTo(300, 100);
