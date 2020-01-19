98fb388fd258823c94e670edea9bd5ea1fdb240e
tangias <tansun@ucdavis.edu>
2020-01-19 00:23:37 -0800

    Geometry Interface

--- Tangia's testing website.js
+++ Tangia's testing website.js
@@ -1,17 +1,31 @@
-//When right triangle button is pressed
-var triangleadded = false
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var c = canvas.getContext('2d');
 
-// //function addtriangle(){
-//     if(triangleadded == false) 
-//     {
+function drawCircle(radius, xPos, yPos)
+{
+    c.fillText("Radius = " + radius, Math.round(xPos) + 0.8 * Math.round(radius), Math.round(yPos) - 0.8 * Math.round(radius), 100);
+
+    c.beginPath();
+    c.arc(xPos, yPos, radius, 0, 2* Math.PI , true);
+    c.stroke();
+}
+
+
+//When right triangle button is pressed
+
+// function addtriangle(){
+//  if(triangleadded == false) 
+//      {
         
 //         var div = document.getElementById("deftri");
-//         var triangle = document.createElement("div");
+//        var triangle = document.createElement("div");
 //         triangle.className = "triangle-up";
-//         triangleadded = true
-//         div.appendChild(triangle)
+//          triangleadded = true
+//          div.appendChild(triangle)
 //     }
-// }
+//  }
 // function addrectangle(){
 //     if(rectangleadded == false)
 // {
@@ -19,6 +33,6 @@ var triangleadded = false
 //     var rectange = document.createElement("div");
 //     rectange.className = "rectangle";
 //     rectangleadded = true
-//     div.appendChild(rectangle)
-//     }
-// }
+//      div.appendChild(rectangle)
+//      }
+//  }
