d97efb15247ee83ea7b97b5396f3580417e9f164
tangias <tansun@ucdavis.edu>
2020-01-18 23:30:53 -0800

    Triangle Creation

--- Tangia's testing website.js
+++ Tangia's testing website.js
@@ -1,15 +1,24 @@
 //When right triangle button is pressed
 var triangleadded = false
 
-
-function addtriangle(){
-    if(triangleadded == false) 
-    {
-        var div = document.getElementById("deftri");
-        var triangle = document.createElement("div");
-        triangle.className = "triangle-up";
-        triangleadded = true
-        div.appendChild(triangle)
-    }
-    
-}
+// //function addtriangle(){
+//     if(triangleadded == false) 
+//     {
+        
+//         var div = document.getElementById("deftri");
+//         var triangle = document.createElement("div");
+//         triangle.className = "triangle-up";
+//         triangleadded = true
+//         div.appendChild(triangle)
+//     }
+// }
+// function addrectangle(){
+//     if(rectangleadded == false)
+// {
+//     var div = document.getElementById("defrec");
+//     var rectange = document.createElement("div");
+//     rectange.className = "rectangle";
+//     rectangleadded = true
+//     div.appendChild(rectangle)
+//     }
+// }
